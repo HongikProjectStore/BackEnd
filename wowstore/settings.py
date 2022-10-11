00b2c2a4ef38d7e5809d9f6e148a3e1d41a78768
sumin-yang224 <kzx1547p@gmail.com>
2022-10-12 02:06:15 +0900

    [fix] ip 변경

--- wowstore/settings.py
+++ wowstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = MY_SECRET_KEY['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '34.234.228.90']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '43.200.205.125']
 
 AUTH_USER_MODEL = 'authentication.User'
 
